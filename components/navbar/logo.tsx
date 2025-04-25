aafdcd617b63f7a7cf435ea469311c85f17e1716
Mumtaz Mert Tekdemir <mumtazmert@gmail.com>
2025-04-25 08:14:19 +0300

    Feat : Logo added

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const Logo = () => {
    return (
       <Link href="/" className="text-2xl font-bold text-primary text-glow">
-         Portfolio
+         <Image src="/logo.svg" alt="Portfolio Logo" width={100} height={100} />
       </Link>
    );
 };
